3a4995ecb3321b64039bf218806fad175bd93f87
신정명 <warning6715@gmail.com>
2024-01-15 19:09:54 +0900

    스킬, 프로젝트 보완

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import '../styles/project.scss'
 import 마켓컬리  from '../assets/market_kurly_main.png'
+import 깃허브 from '../assets/github_logo.png'
 
 export default function Project(){
     
@@ -20,7 +21,8 @@ export default function Project(){
                                 리액트를 이용한 프로젝트<br/>
                                 scss, axios, redux, json, php, mysql 사용
                             </div>
-                            <button>이동하기</button>
+                            <a href="https://github.com/mungmung2j/kurly-final"><img src={깃허브} alt="" /></a>
+                            <a className="kurly-link" href="https://sjm6715.com/kurly_week_11/">이동하기</a>
                         </div>
                     </div>
                 </div>
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -5,7 +5,41 @@ export default function Skill(){
     return(
         <div id="skill">
             <section id="section2">
-                ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+                <h2>SKill</h2>
+                <div className="skill-div">
+                    <div>
+                        <h3>HTML/CSS</h3>
+                        <div>
+                            <p>HTML 태그를 활용한 웹 페이지 화면 구현 가능</p>
+                            <p>웹 접근성을 고려한 마크업 가능</p>
+                            <p>flex 속성 활용 가능</p>
+                            <p>grid 속성으로 반응형 프로젝트 경험</p>
+                            <p>SCSS 활용 가능</p>
+                        </div>
+                    </div>
+                    <div>
+                        <h3>JavaScript</h3>
+                        <p>JavaScript를 활용하여 동적인 웹 페이지 구축 가능</p>
+                        <p>기본문법 if/for/함수/변수/배열 사용 가능</p>
+                        <p>바닐라 프로젝트로 마켓컬리 사이트 구현 경험</p>
+                    </div>
+                    <div>
+                        <h3>React</h3>
+                        <p>React 개발환경 설정, JSX 문법 사용과 컴포넌트 작성 가능</p>
+                        <p>State, props 개념과 데이터 흐름 이해 가능</p>
+                        <p>Axios를 활용해 DB와 통신 가능</p>
+                        <p>Redux 활용 가능</p>
+                    </div>
+                    <div>
+                        <h3>Git/Github</h3>
+                        <p>Git/Github를 활용 해 프로젝트 소스 이력 관리 가능</p>
+                    </div>
+                    <div>
+                        <h3>Vue</h3>
+                        <p>Vue 개발환경 설정 가능</p>
+                        <p>Vue3를 사용해 농심 프로젝트 구현 경험</p>
+                    </div>
+                </div>
             </section>
         </div>
     )
